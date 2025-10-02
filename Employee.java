f94de5252b0cb77ada64b6057253f8bb90645b33
Karthik4202 <kamunikarthik1234@gmail.com>
2025-10-02 15:54:35 +0530

    Added hello method

--- Employee.java
+++ Employee.java
@@ -2,4 +2,8 @@ public class Employee{
     public static void main(String[] args){
         System.out.println("hello");
     }
+
+    public String hello(String name){
+        return "Hello "+name;
+    }
 }
